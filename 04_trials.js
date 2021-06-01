5d537054344e110cf0fce4314c3f9e6905b05f98
Nora <noora.maleki@gmail.com>
2021-06-02 00:23:33 +0200

    replacing the forced choice with key press; still needs debugging

--- 04_trials.js
+++ 04_trials.js
@@ -1,8 +1,8 @@
 // In this file you can specify the trial data for your experiment
 
 
-const trial_info = {
-    forced_choice: [
+/*const trial_info = {
+    key_pressed: [
         {
             question: "What's on the bread?",
             picture: "images/question_mark_02.png",
@@ -16,6 +16,29 @@ const trial_info = {
             option1: "shiny",
             option2: "rainbow",
             correct: "shiny"
+        }
+    ]
+};*/
+
+const trial_info = {
+    key_pressed: [
+        {
+            question: "Are these the same object or different objects?",
+            picture: 'images/1_50_different.jpg',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'different'
+        },
+        {
+            question: "Are these the same object or different objects?",
+            picture: 'images/1_50_same.jpg',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'same'
         }
     ]
 };
