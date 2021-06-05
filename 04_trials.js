ccefed4fd404cd638af4b29990ac002bd889323a
Nora <noora.maleki@gmail.com>
2021-06-05 16:18:27 +0200

    added the trials for the practice session

--- 04_trials.js
+++ 04_trials.js
@@ -1,6 +1,143 @@
 // In this file you can specify the trial data for your experiment
 
-// todo implement the stimuli for practice trials
+var question = 'Are these objects the same object or different objects?';
+
+const trial_info_practice = {
+    key_press: [
+        {
+            question: question,
+            picture: 'images/13_50_different.jpg',
+            picture_number: 13,
+            degree_of_rotation: 50,
+            expected: 'different',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/13_50_same.jpg',
+            picture_number: 13,
+            expected: 'same',
+            degree_of_rotation: 50,
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/13_150_different.jpg',
+            picture_number: 13,
+            degree_of_rotation: 150,
+            expected: 'different',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/13_150_same.jpg',
+            picture_number: 13,
+            degree_of_rotation: 150,
+            expected: 'same',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/14_50_different.jpg',
+            picture_number: 14,
+            degree_of_rotation: 50,
+            expected: 'different',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/14_50_same.jpg',
+            picture_number: 14,
+            degree_of_rotation: 50,
+            expected: 'same',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/14_150_different.jpg',
+            picture_number: 14,
+            degree_of_rotation: 150,
+            expected: 'different',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/14_150_same.jpg',
+            picture_number: 14,
+            degree_of_rotation: 150,
+            expected: 'same',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/15_50_different.jpg',
+            picture_number: 15,
+            degree_of_rotation: 50,
+            expected: 'different',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/15_50_same.jpg',
+            picture_number: 15,
+            degree_of_rotation: 50,
+            expected: 'same',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/15_150_different.jpg',
+            picture_number: 15,
+            degree_of_rotation: 150,
+            expected: 'different',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        },
+        {
+            question: question,
+            picture: 'images/15_150_same.jpg',
+            picture_number: 15,
+            degree_of_rotation: 150,
+            expected: 'same',
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different'
+        }
+    ]
+};
 
 // todo implement the rest of the stimuli for main trials
 const trial_info = {
