1a67f8a57248baefd941208896927dfff024442c
Nora <noora.maleki@gmail.com>
2021-06-06 12:59:20 +0200

    minor change; attempt to fix data saving

--- 06_main.js
+++ 06_main.js
@@ -26,7 +26,7 @@ $("document").ready(function() {
         // Here, you can specify all information for the deployment
         deploy: {
             experimentID: "219",
-            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/219",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
